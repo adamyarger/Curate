b547606ae6f1a1c7887c39d59944aa70c77809b7
adamyarger <yargeradam@gmail.com>
2015-02-10 14:25:01 -0600

    added static pages controller

---
+++ app/controllers/static_pgaes_controller.rb
@@ -0,0 +1,7 @@
+class StaticPgaesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'static_pgaes/home'
+
+  get 'static_pgaes/about'
+
   devise_for :users
   root 'welcome#index'
   
