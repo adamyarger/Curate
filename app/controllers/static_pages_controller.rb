fdb98ef6729549a0b87a271a3d52c78829228b20
adamyarger <yargeradam@gmail.com>
2015-02-24 19:04:10 -0600

    rendered next web templeate

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
   	if user_signed_in?
+  		@message = Message.new
+  		@user = current_user.following
 	  	@post  = current_user.posts.build
       	@feed_items = current_user.feed.paginate(:page => params[:page], :per_page => 10)
 	end
@@ -8,4 +10,6 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+
 end
