5e6627885d59c024a3026968e94aea978dd5e87e
adamyarger <yargeradam@gmail.com>
2015-03-01 18:06:02 -0600

    now its all working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   	if user_signed_in?
 	  	@post  = current_user.posts.build
       @feed_items = current_user.feed.paginate(:page => params[:page], :per_page => 10)
-      @recipients = Recipient.where(:user_id => current_user)
+      
 	end
   end
 
