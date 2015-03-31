30d44909b2b61cbe25ea738a279eafd3b73f76ec
adamyarger <yargeradam@gmail.com>
2015-03-31 17:07:57 -0500

    suvview for post form is working

--- app/assets/javascripts/views/posts/posts_index.js
+++ app/assets/javascripts/views/posts/posts_index.js
@@ -2,7 +2,12 @@ window.Curate.Views.PostsIndex = Backbone.CompositeView.extend({
 	template: JST['posts/index'],
 
 	initialize: function(options){
-		this.listenTo(this.collection, 'sync unshift', this.render)
+		this.listenTo(this.collection, 'sync add', this.render);
+
+		var postNewView = new Curate.Views.PostsNew({
+			post: this.model
+		});
+		this.addSubview('#post-new', postNewView);
 	},
 
 	render: function(){
@@ -12,9 +17,7 @@ window.Curate.Views.PostsIndex = Backbone.CompositeView.extend({
 
 		this.$el.html(renderedContent);
 
-		var newPost = new Curate.Views.PostsNew();
-		$('.container').prepend(newPost.render().$el);
-
+		this.renderSubviews();
 
 		return this;
 	}
