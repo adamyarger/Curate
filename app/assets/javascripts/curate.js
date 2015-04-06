255927b690c9812fa1f7aa0114358ec6af0f29d5
adamyarger <yargeradam@gmail.com>
2015-04-06 09:49:28 -0500

    good fall back point

--- app/assets/javascripts/curate.js
+++ app/assets/javascripts/curate.js
@@ -9,6 +9,9 @@ window.Curate = {
 		var rightNav = new Curate.Views.RightNav();
 		$('#right-nav').append(rightNav.render().$el);
 
+		var messageForm = new Curate.Views.MessageNew();
+		$('#stream').append(messageForm.render().$el);
+
 		new Curate.Routers.AppRouter();
 		Backbone.history.start();
 	}
--- app/assets/javascripts/views/messages/message_new.js
+++ app/assets/javascripts/views/messages/message_new.js
@@ -2,31 +2,31 @@ window.Curate.Views.MessageNew = Backbone.View.extend({
 
 	template: JST['messages/new'],
 
-	events: {
-		'submit form': 'submit'
-	},
+	// events: {
+	// 	'submit form': 'submit'
+	// },
 
 	render: function(){
 		var renderedContent = this.template();
 		this.$el.html(renderedContent);
 
 		return this;
-	},
-
-	submit: function(event){
-		event.preventDefault();
-
-		var params = $(event.currentTarget).serializeJSON()['post'];
-		var newPost = new Curate.Models.Post(params);
-		newPost.save({}, {
-			success: function(){
-				Curate.Collections.posts.unshift(newPost);
-				Curate.Collections.feed.fetch({
-				  data: { page: 1 }
-				});
-				Curate.Flash.success('Post success!');
-			}
-		});
 	}
+
+	// submit: function(event){
+	// 	event.preventDefault();
+
+	// 	var params = $(event.currentTarget).serializeJSON()['post'];
+	// 	var newPost = new Curate.Models.Post(params);
+	// 	newPost.save({}, {
+	// 		success: function(){
+	// 			Curate.Collections.posts.unshift(newPost);
+	// 			Curate.Collections.feed.fetch({
+	// 			  data: { page: 1 }
+	// 			});
+	// 			Curate.Flash.success('Post success!');
+	// 		}
+	// 	});
+	// }
 
 });
