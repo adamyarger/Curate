f4ae0e99c08046b1a016e4fe3e2a32e2545d9e39
adamyarger <yargeradam@gmail.com>
2015-04-07 13:41:48 -0500

    like model is set up

---
+++ app/models/like.rb
@@ -0,0 +1,14 @@
+class Like < ActiveRecord::Base
+  
+
+  validates_uniqueness_of :user_id, scope: [:likeable_id, :likeable_type]
+
+  belongs_to :user, class_name: "User", foreign_key: :user_id, primary_key: :id
+  
+
+  belongs_to :likeable, polymorphic: true, foreign_key: :likeable_id, primary_key: :id
+  
+
+  # has_many :notifications, as: :notifiable, foreign_key: :notifiable_id, primary_key: :id
+  
+end
