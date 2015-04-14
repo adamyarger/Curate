2ea6f8ff3e4510d0aa4584e11b4ce5b8e8ea66e1
adamyarger <yargeradam@gmail.com>
2015-04-14 16:11:07 -0500

    its working the preview that is

--- app/assets/javascripts/views/posts/posts_feed.js
+++ app/assets/javascripts/views/posts/posts_feed.js
@@ -6,11 +6,9 @@ window.Curate.Views.PostsIndex = Backbone.CompositeView.extend(
 			this.listenTo(this.model, 'sync add', this.render);
 			this.listenTo(this.model.posts(), 'add', this.addPost);
 			// this.listenTo(this.model.posts(), 'remove', this.removePost);
-
 			// this.model.posts().each(this.addPost.bind(this));
-			$('#post-body').preview({key:'77b9e769557a490e9e2ef087981131c3'})
-
-			$('#post-body').on('loading', function(){$('.loading').show()});
+			// $('#post-body').preview({key:'77b9e769557a490e9e2ef087981131c3'})
+			// $('#post-body').on('loading', function(){$('.loading').show()});
 
 			setInterval(this.nextPosts.bind(this), 1000);
 
