a5ad9fc92e997ff18eb240ca0249cd645d7a1aa1
adamyarger <yargeradam@gmail.com>
2015-04-28 11:36:37 -0500

    work you fucker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
+      :s3_host_name => 's3-us-west-2.amazonaws.com',
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
